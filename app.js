6f673cd73446077b8ccf181e610f8e599877a0b0
HARINI SRI S <85395304+SHS-coder@users.noreply.github.com>
2021-08-08 06:37:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Web-Server code
+
+const express = require('express');
+const app = express();
+
+// Set the view engine to EJS
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+// render the separate pages (also allowing for absolute urls)
+
+app.get('/', (req, res) => {
+	res.render('pages/index');
+})
+
+app.get('/cam', (req, res) => {
+	res.render('pages/cam');
+})
+
+app.get('/ar', (req, res) => {
+	res.render('pages/ar');
+})
+
+app.get('/ttslearning', (req, res) => {
+	res.render('pages/ttslearning');
+})
+
+// Initiate the server on port 8080
+
+app.listen(8080)
